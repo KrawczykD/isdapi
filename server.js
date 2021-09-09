f9a11ca77bc96c7018a8fc508d966ef043066359
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2021-09-09 16:20:44 +0100

    add routes for server

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+
+server.use(jsonServer.rewriter({
+    "/create/products": "/createProducts",
+    "/fusion/products": "/fusionProducts"
+  }))
+
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+jsonServer.rewriter
