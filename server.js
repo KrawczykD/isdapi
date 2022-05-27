bc160bc19bd65087ffb571e19e9477b58868fba9
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2022-05-27 08:30:46 +0100

    fix paths

--- server.js
+++ server.js
@@ -6,17 +6,17 @@
 
 
   server.use(jsonServer.rewriter({
-      "/": "/home",
-      "/brand/:brand": "/:brand",
-      "/brand/:brand/cat/:cat": "/products?brand=:brand&cat=:cat",
-      "/brand/:brand/cat/:cat/productid/:id": "/products?brand=:brand&cat=:cat&id=:id",
+      // "/": "/home",
+      // "/brand/:brand": "/:brand",
+      // "/brand/:brand/cat/:cat": "/products?brand=:brand&cat=:cat",
+      // "/brand/:brand/cat/:cat/productid/:id": "/products?brand=:brand&cat=:cat&id=:id",
 
-      "/postcoderhints/:option":"/postcoderhints?Message_like=:option",
-      "/postcoderfull/:option":"/postcoderfull?Message_like=:option",
-      "/coordinates":"/coordinates",
-      "/recentUpdatesSingle":"/single",
-      "/recentUpdatesFalse":"/false",
-      "/recentUpdatesNone":"none"
+      // "/postcoderhints/:option":"/postcoderhints?Message_like=:option",
+      // "/postcoderfull/:option":"/postcoderfull?Message_like=:option",
+      // "/coordinates":"/coordinates",
+      "/Single":"/single",
+      "/False":"/false",
+      "/None":"/none"
     }))
 
   server.use(middlewares);
